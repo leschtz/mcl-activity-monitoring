dbd5cd3669b681ec1e79275056959ebfeb8a6a04
Lukas Schuetz <leschtz@icloud.com>
2022-04-27 15:37:17 +0200

    implementing a strategy, how to handle multiple data entries

---
+++ app/src/main/java/com/example/activitymonitoring/DataStrategy/AverageStrategy.java
@@ -0,0 +1,27 @@
+package com.example.activitymonitoring.DataStrategy;
+
+import java.util.List;
+import java.util.Arrays;
+
+public class AverageStrategy implements DataStrategy {
+
+
+    @Override
+    public double[] execute(List<Double[]> data) {
+        if (data.size() == 0) {
+            return null;
+        }
+
+        double[] result = new double[data.get(0).length];
+        Arrays.fill(result, 0);
+
+        // calculate the average for every entry and return
+        for (Double[] entry : data) {
+            for (int i = 0; i < entry.length; i++) {
+                result[i] = (result[i] + entry[i]) / 2;
+            }
+        }
+
+        return result;
+    }
+}
---
+++ app/src/main/java/com/example/activitymonitoring/DataStrategy/Strategy.java
@@ -0,0 +1,7 @@
+package com.example.activitymonitoring.DataStrategy;
+
+import java.util.List;
+
+public interface DataStrategy {
+    public double[] execute(List<Double[]> data);
+}
