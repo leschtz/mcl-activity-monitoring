8e8a60c9be5442a2b7bf1f2c2aa03f4111999fbf
Lukas Schuetz <leschtz@icloud.com>
2022-05-03 19:46:01 +0200

    fixing initial case for AverageStrategy

--- app/src/main/java/com/example/activitymonitoring/DataStrategy/AverageStrategy.java
+++ app/src/main/java/com/example/activitymonitoring/DataStrategy/AverageStrategy.java
@@ -36,15 +36,21 @@ public class AverageStrategy implements Strategy {
         Arrays.fill(result, 0);
 
         // calculate the average for every entry and return
-        for (float[] entry : data) {
+        for (int cnt = 0; cnt < data.size(); cnt++) {
 
+            //for (float[] entry : data) {
+            float[] entry = data.get(cnt);
             if (entry == null) {
                 continue;
             }
 
             for (int i = 0; i < entry.length; i++) {
+                if (cnt == 0) {
+                    result[i] = ((double) entry[i]);
+                } else {
+                    result[i] = (result[i] + (double) entry[i]) / 2;
 
-                result[i] = (result[i] + (double) entry[i]) / 2;
+                }
                 //System.out.println("excute loop: " + result[i]);
             }
         }
