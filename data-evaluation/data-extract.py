537b68ca3e953070d9577cfcdff74824e34b2750
Lukas Schuetz <leschtz@icloud.com>
2022-05-19 17:28:31 +0200

    changing structure of tool to work again

--- data-evaluation/data-extract.py
+++ data-evaluation/data-extract.py
@@ -1,31 +1,27 @@
 import argparse
-from email import header
 import pathlib
 import os
-
 
 def handle_args():
     parser = argparse.ArgumentParser()
     parser.add_argument("file", help="logfile to be extracted.")
     parser.add_argument("outfile", help="output destination")
     parser.add_argument("--keywords", "-k", action="append", help="Keywords which should be in a line")
-    parser.add_argument("--label", "-l", action="append", help="Specifies label", type=str)
     parser.add_argument("--force", "-f", action="store_true", help="overwrite existing files")
+    parser.add_argument("--label", "-l", help="set a label")
 
     return parser.parse_args()
-
 
 def is_parseable_line(line, keywords):
     for keyword in keywords:
         if keyword.lower() in line.lower():
             return True
-
+    
     return False
-
 
 def split_line(line):
     data = line.split(";")
-
+    
     sensor_name = data[0]
     timestamp = data[1]
     values = data[2:]
@@ -38,14 +34,14 @@ def main():
     file = pathlib.Path(args.file)
     if not file.exists():
         print(f"File {file} does not exist.")
-        return
+        return 
 
     outfile = pathlib.Path(args.outfile)
-    if not outfile.exists():
-        outfile.touch()
-    #     print(f"Outfile {outfile} does already exist.")
-    #     return
-
+    if outfile.exists() and not args.force:
+        print(f"Outfile {outfile} does already exist.")
+        return 
+    outfile.touch()
+    
     lines = file.read_text()
 
     output_data = {}
@@ -56,42 +52,38 @@ def main():
         if not is_parseable_line(line, args.keywords):
             continue
         name, _, values = split_line(line)
-        output_data[name] = ["0"] * (len(values) - 1)
+        output_data[name] = ["0"] * (len(values)-1)
 
     # create the header string
-    # header_string = ""
-    # for key, value in output_data.items():
-    #     header_string += (f"{key};")
-    #     for index in range(len(value)):
-    #         header_string += (f"{index};")
-    # header_string += os.linesep
-    # final_output.append(header_string)
+    header_string = ""
+    for key, value in output_data.items():
+        header_string += (f"{key};")
+        for index in range(len(value)):
+            header_string += (f"{index};")
+    header_string += os.linesep
+    final_output.append(header_string)
 
     # fill in the data
     for line in lines.splitlines():
+        if not is_parseable_line(line, args.keywords):
+            continue
         name, _, values = split_line(line)
         output_data[name] = values
 
         data_string = ""
         for _, value in output_data.items():
-            data_string += ",".join(value)
+            data_string += ";".join(values)
 
-        if (len(value) == 3):
-            data_string += ","
-        data_string += ",".join(args.label)
-        #data_string += ","
-        data_string += "\n"
+        if args.label:
+            data_string += f"{args.label}"
 
+        data_string += os.linesep
+        #print(data_string)
         final_output.append(data_string)
 
-    if not args.force:
-        with open(outfile, "a") as out:
-            for line in final_output:
-                out.write(line)
-    else:
-        with open(outfile, "w") as out:
-            for line in final_output:
-                out.write(line)
+    with open(outfile, "w") as out:
+        for line in final_output:
+            out.write(line)
 
 
 if __name__ == "__main__":
